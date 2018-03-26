3348c1738d77f024e11336691d00a6301b010bc8
sobaka1 <alexpad566@gmail.com>
2018-03-25 17:42:42 -0700

    removed 'var' in front of 'names', since this array is already declared in index.html

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -35,7 +35,8 @@ WARNING!!! WARNING!!!
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
 (function () {
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+   // since 'names' array is already declared in index.html, don't have to use 'var' to declare it again
+   names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
